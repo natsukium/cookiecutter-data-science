989e920a514c98f7b97e7c32c18167814f9bd1c1
OTABI Tomoya <tomoya.otabi@gmail.com>
2023-03-04 17:04:08 +0900

    test: add sample test (#7)

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -178,6 +178,7 @@ class TestCookieSetup(object):
             'src/features',
             'src/models',
             'src/visualization',
+            'tests',
         ]
 
         ignored_dirs = [
